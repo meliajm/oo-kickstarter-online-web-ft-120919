4ebc169f87ec203c7e5544c07af8e490ca33cc23
melia miller <meliajm@gmail.com>
2019-12-21 03:33:15 +0000

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,22 @@
+require 'pry'
+
+class Backer 
+  
+  attr_accessor :name
+  attr_accessor :backed_projects
+  
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+  
+  def back_project(project)
+    self.backed_projects << project
+    project.backers << self
+    # binding.pry 
+  end
+  
+  
+   
+   
+end
